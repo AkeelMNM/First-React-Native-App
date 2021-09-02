983a5be378fe3daa9b60f9b10719fc3077a51e67
Mohamed Akeel <akeel1447@gmail.com>
2021-09-02 11:35:34 +0530

    Service created for api calls

---
+++ src/service/ApiService.js
@@ -0,0 +1,29 @@
+
+const AGE_OF_EMPIRES_II_API_URL = 'https://age-of-empires-2-api.herokuapp.com/api/v1';
+
+class ApiService{
+
+    getResourcesFromApi = (resource) =>{
+        return fetch(AGE_OF_EMPIRES_II_API_URL + '/' + type)
+            .then((response) =>{
+                return response.json();
+            })
+            .catch((error) =>{
+                console.error(error);
+            })
+    }
+
+    getResourceFromApi = (resource, id) =>{
+            return fetch(AGE_OF_EMPIRES_II_API_URL + '/' + resource + '/' + id)
+                .then((response) =>{
+                    return response.json();
+                })
+                .catch((error) =>{
+                    console.error(error);
+                })
+    }
+
+
+}
+
+export default new ApiService();
